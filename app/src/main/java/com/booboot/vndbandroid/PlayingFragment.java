00f08bc0ab4443e22bb7c5a451136de714fe99fb
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2016-03-10 23:51:44 +0100

    no message

--- app/src/main/java/com/booboot/vndbandroid/PlayingFragment.java
+++ app/src/main/java/com/booboot/vndbandroid/PlayingFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,10 @@ import com.dexafree.materialList.card.OnActionClickListener;
 import com.dexafree.materialList.card.action.TextViewAction;
 import com.dexafree.materialList.view.MaterialListView;
 import com.squareup.picasso.RequestCreator;
+
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
 
 /**
  * Created by od on 09/03/2016.
@@ -71,6 +76,44 @@ public class PlayingFragment extends Fragment {
         mListView.getAdapter().add(card);
         mListView.scrollToPosition(0);
 
+        /* [TODO] Put this in a pretty class (see vndb.py for reference) */
+        new Thread() {
+            public void run() {
+                String hostName = "api.vndb.org";
+                int portNumber = 19534;
+
+                try {
+                    Socket kkSocket = new Socket(hostName, portNumber);
+                    kkSocket.setKeepAlive(true);
+                    OutputStream out = kkSocket.getOutputStream();
+                    InputStreamReader in = new InputStreamReader(kkSocket.getInputStream());
+                    String fromServer = "", fromUser;
+                    /* [IMPORTANT] Always put this character at the end of a message */
+                    char EOM = 0x04;
+                    /* [IMPORTANT] Remember to always put the username in lowercase here */
+                    fromUser = "login {\"protocol\":1,\"client\":\"test\",\"clientver\":0.1,\"username\":\"turboblaster\",\"password\":\"turlututu\"}" +
+                            EOM;
+                    Log.d("D", "Client: " + fromUser);
+                    out.write(fromUser.getBytes("UTF-8"));
+
+                    int read = in.read();
+                    while (read != 10 && read > -1) {
+                        fromServer += String.valueOf((char) read);
+                        read = in.read();
+
+                        Log.e("D", "Server: " + fromServer);
+                    }
+
+                    in.close();
+                    out.close();
+                    kkSocket.close();
+                    Log.e("D", "Server 2: " + fromServer);
+                } catch (Exception e) {
+                    Log.e("D", "ERREUR :(");
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
         return rootView;
     }
