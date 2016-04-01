04a417b2d61f96db7077137e87f2998c46ec033a
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2016-04-01 20:29:59 +0200

    [+] Added filter and search buttons to action bar.

--- app/src/main/java/com/booboot/vndbandroid/activity/MainActivity.java
+++ app/src/main/java/com/booboot/vndbandroid/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -60,21 +61,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_filter:
+                Log.d("D", "ACTION FILTER");
+                return true;
+
+            case R.id.action_search:
+                Log.d("D", "ACTION SEARCH");
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
