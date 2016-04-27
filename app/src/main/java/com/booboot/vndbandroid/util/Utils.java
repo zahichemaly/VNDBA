789b94925c0d7d78f9bbee96dc7f48366794f21e
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2016-04-27 20:15:34 +0200

    Refactor. [-] "Bitmap" class (obsolete). [#] "StringUtils" + "DateUtils" -> "Utils".

---
+++ app/src/main/java/com/booboot/vndbandroid/util/Utils.java
@@ -0,0 +1,57 @@
+package com.booboot.vndbandroid.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.booboot.vndbandroid.api.bean.Links;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by od on 03/04/2016.
+ */
+public class Utils {
+    public static boolean isNumeric(String str) {
+        NumberFormat formatter = NumberFormat.getInstance();
+        ParsePosition pos = new ParsePosition(0);
+        formatter.parse(str, pos);
+        return str.length() == pos.getIndex();
+    }
+
+    public static String booleanToString(boolean bool) {
+        return bool ? "Yes" : "No";
+    }
+
+    public static String capitalize(String s) {
+        if (s == null) return null;
+        if (s.length() == 1) {
+            return s.toUpperCase();
+        }
+        if (s.length() > 1) {
+            return s.substring(0, 1).toUpperCase() + s.substring(1);
+        }
+        return "";
+    }
+
+    public static String getDate(String date) {
+        if (date == null) {
+            return "Unknown";
+        } else try {
+            Date released = new SimpleDateFormat("yyyy-MM-dd", Locale.US).parse(date);
+            return new SimpleDateFormat("d MMMM yyyy", Locale.US).format(released);
+        } catch (ParseException e) {
+            return date;
+        }
+    }
+
+    public static void openInBrowser(Context context, String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        context.startActivity(browserIntent);
+    }
+}
