2f0b5a336f72fd6efb7cd2593c1856db6312fe5f
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2016-04-30 21:58:00 +0200

    Improved image display. [Fix] Images are no longer overscaled to fit the screen, but just take the space they need instead. [Fix] Enabled large heap to allow huge image loading without OOM. [Fix] Lightbox images are now displayed only when they are loaded, to better prevent loading problems.

--- app/src/main/java/com/booboot/vndbandroid/util/Lightbox.java
+++ app/src/main/java/com/booboot/vndbandroid/util/Lightbox.java
@@ -2,14 +2,17 @@ package com.booboot.vndbandroid.util;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.booboot.vndbandroid.R;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 /**
  * Created by od on 26/03/2016.
@@ -25,21 +28,39 @@ public class Lightbox {
                 dialog.setCancelable(true);
 
                 final ImageView lightbox = (ImageView) dialog.findViewById(R.id.lightboxView);
-                ImageLoader.getInstance().displayImage(url, lightbox);
-
-                WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
-                params.width = WindowManager.LayoutParams.MATCH_PARENT;
-                params.height = WindowManager.LayoutParams.MATCH_PARENT;
-                dialog.getWindow().setAttributes(params);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-                dialog.setCanceledOnTouchOutside(true);
-                lightbox.setOnClickListener(new View.OnClickListener() {
+                ImageLoader.getInstance().loadImage(url, new ImageLoadingListener() {
                     @Override
-                    public void onClick(View v) {
+                    public void onLoadingStarted(String imageUri, View view) {
+                    }
+
+                    @Override
+                    public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
                         dialog.dismiss();
+                        ImageLoader.getInstance().cancelDisplayTask(lightbox);
+                        Toast.makeText(context, "Could not load image : " + failReason.getType(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                        lightbox.setMinimumWidth(Pixels.px(loadedImage.getWidth(), context));
+                        lightbox.setMinimumHeight(Pixels.px(loadedImage.getHeight(), context));
+                        lightbox.setImageBitmap(loadedImage);
+                        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                        dialog.setCanceledOnTouchOutside(true);
+                        lightbox.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ImageLoader.getInstance().cancelDisplayTask(lightbox);
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
+                    }
+
+                    @Override
+                    public void onLoadingCancelled(String imageUri, View view) {
                     }
                 });
-                dialog.show();
             }
         });
     }
