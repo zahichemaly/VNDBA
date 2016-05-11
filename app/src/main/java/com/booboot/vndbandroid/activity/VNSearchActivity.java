e1503a3c24b43ad64713062928b70dafa46fd067
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2016-05-11 20:44:00 +0200

    [+] #35 : Search page : displaying more results if scroll reaches bottom.

--- app/src/main/java/com/booboot/vndbandroid/activity/VNSearchActivity.java
+++ app/src/main/java/com/booboot/vndbandroid/activity/VNSearchActivity.java
@@ -8,18 +8,23 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.booboot.vndbandroid.R;
 import com.booboot.vndbandroid.adapter.materiallistview.MaterialListView;
+import com.booboot.vndbandroid.api.Cache;
 import com.booboot.vndbandroid.api.VNDBServer;
 import com.booboot.vndbandroid.api.bean.Item;
 import com.booboot.vndbandroid.api.bean.Options;
-import com.booboot.vndbandroid.api.Cache;
 import com.booboot.vndbandroid.factory.VNCardFactory;
 import com.booboot.vndbandroid.util.Callback;
 import com.booboot.vndbandroid.util.SettingsManager;
@@ -28,6 +33,10 @@ import com.dexafree.materialList.listeners.RecyclerItemClickListener;
 
 public class VNSearchActivity extends AppCompatActivity {
     private MaterialListView materialListView;
+    private ProgressBar progressBar;
+    private int currentPage;
+    private String currentQuery;
+    private boolean moreResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,7 @@ public class VNSearchActivity extends AppCompatActivity {
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         materialListView = (MaterialListView) findViewById(R.id.materialListView);
         /* [Fix] Set the background color to match the default one (not the case by default) */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -46,6 +56,23 @@ public class VNSearchActivity extends AppCompatActivity {
         } else {
             materialListView.getRootView().setBackgroundColor(getResources().getColor(R.color.windowBackground));
         }
+
+        materialListView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0 && !progressBar.isShown() && moreResults && recyclerView.getLayoutManager() instanceof LinearLayoutManager) {
+                    int visibleItemCount = recyclerView.getLayoutManager().getChildCount();
+                    int totalItemCount = recyclerView.getLayoutManager().getItemCount();
+                    int pastVisiblesItems = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+
+                    if (visibleItemCount + pastVisiblesItems >= totalItemCount) {
+                        progressBar.setVisibility(View.VISIBLE);
+                        currentPage++;
+                        loadResults(false);
+                    }
+                }
+            }
+        });
 
         SwipeRefreshLayout refreshLayout = (SwipeRefreshLayout) findViewById(R.id.refreshLayout);
         refreshLayout.setEnabled(false);
@@ -74,31 +101,9 @@ public class VNSearchActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 searchView.clearFocus();
-                VNDBServer.get("vn", Cache.VN_FLAGS, "(search ~ \"" + query.trim() + "\")", Options.create(false, false), VNSearchActivity.this, new Callback() {
-                    @Override
-                    protected void config() {
-                        materialListView.getAdapter().clearAll();
-
-                        for (final Item vn : results.getItems()) {
-                            VNCardFactory.buildCard(VNSearchActivity.this, vn, materialListView);
-                        }
-
-                        materialListView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(Card card, int position) {
-                                Intent intent = new Intent(VNSearchActivity.this, VNDetailsActivity.class);
-                                intent.putExtra(VNTypeFragment.VN_ARG, (Item) card.getTag());
-                                startActivity(intent);
-                            }
-
-                            @Override
-                            public void onItemLongClick(Card card, int position) {
-                                Log.d("LONG_CLICK", card.getProvider().getTitle());
-                            }
-                        });
-                    }
-                }, Callback.errorCallback(VNSearchActivity.this));
-
+                currentPage = 1;
+                currentQuery = query;
+                loadResults(true);
                 return true;
             }
 
@@ -109,6 +114,44 @@ public class VNSearchActivity extends AppCompatActivity {
         });
 
         return true;
+    }
+
+    private void loadResults(final boolean clearData) {
+        VNDBServer.get("vn", Cache.VN_FLAGS, "(search ~ \"" + currentQuery.trim() + "\")", Options.create(currentPage, 25, null, false, false, false), VNSearchActivity.this, new Callback() {
+            @Override
+            protected void config() {
+                moreResults = results.isMore();
+                if (clearData)
+                    materialListView.getAdapter().clearAll();
+
+                for (final Item vn : results.getItems()) {
+                    VNCardFactory.buildCard(VNSearchActivity.this, vn, materialListView);
+                }
+
+                materialListView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(Card card, int position) {
+                        Intent intent = new Intent(VNSearchActivity.this, VNDetailsActivity.class);
+                        intent.putExtra(VNTypeFragment.VN_ARG, (Item) card.getTag());
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onItemLongClick(Card card, int position) {
+                    }
+                });
+
+                if (currentPage == 1)
+                    materialListView.scrollToPosition(0);
+                progressBar.setVisibility(View.GONE);
+            }
+        }, new Callback() {
+            @Override
+            public void config() {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(VNSearchActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
