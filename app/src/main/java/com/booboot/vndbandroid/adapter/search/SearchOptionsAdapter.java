94eaa8ca50166a4687899efd9f1102ea702191ad
Thomas HERBETH <thomas.herbeth@gmail.com>
2016-06-24 20:47:37 +0200

    [#] #54 : Redesigned tag form : floating hint, green/red icon instead of labels, dropdown "include all/at least one tags", rounded-corner background, shadow.

--- app/src/main/java/com/booboot/vndbandroid/adapter/search/SearchOptionsAdapter.java
+++ app/src/main/java/com/booboot/vndbandroid/adapter/search/SearchOptionsAdapter.java
@@ -1,17 +1,23 @@
 package com.booboot.vndbandroid.adapter.search;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.ListPopupWindow;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,6 +27,7 @@ import com.booboot.vndbandroid.api.bean.Tag;
 import com.booboot.vndbandroid.view.TagAutoCompleteView;
 import com.tokenautocomplete.FilteredArrayAdapter;
 import com.tokenautocomplete.TokenCompleteTextView;
+import com.wrapp.floatlabelededittext.FloatLabeledEditText;
 
 /**
  * Created by od on 21/06/2016.
@@ -56,6 +63,55 @@ public class SearchOptionsAdapter extends BaseExpandableListAdapter {
             excludeTagsInput = (TagAutoCompleteView) convertView.findViewById(R.id.excludeTagsInput);
             initCompletionView(includeTagsInput, VNSearchActivity.INCLUDE_TAGS_STATE);
             initCompletionView(excludeTagsInput, VNSearchActivity.EXCLUDE_TAGS_STATE);
+
+            final FloatLabeledEditText includeTagsFloatingLabel = (FloatLabeledEditText) convertView.findViewById(R.id.includeTagsFloatingLabel);
+            final ImageView includeTagsIcon = (ImageView) convertView.findViewById(R.id.includeTagsIcon);
+            final ImageView includeTagsDropdown = (ImageView) convertView.findViewById(R.id.includeTagsDropdown);
+            ImageView excludeTagsIcon = (ImageView) convertView.findViewById(R.id.excludeTagsIcon);
+            includeTagsIcon.setColorFilter(context.getResources().getColor(R.color.green), PorterDuff.Mode.SRC_ATOP);
+            includeTagsDropdown.setColorFilter(context.getResources().getColor(R.color.green), PorterDuff.Mode.SRC_ATOP);
+            excludeTagsIcon.setColorFilter(context.getResources().getColor(R.color.red), PorterDuff.Mode.SRC_ATOP);
+
+            LinearLayout includeTagsLayout = (LinearLayout) convertView.findViewById(R.id.includeTagsLayout);
+            includeTagsLayout.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View arg0, MotionEvent arg1) {
+                    switch (arg1.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            includeTagsIcon.setAlpha(0.9f);
+                            includeTagsDropdown.setAlpha(0.9f);
+                            break;
+
+                        case MotionEvent.ACTION_UP:
+                            includeTagsIcon.setAlpha(0.4f);
+                            includeTagsDropdown.setAlpha(0.4f);
+                            PopupMenu popup = new PopupMenu(context, includeTagsDropdown);
+                            MenuInflater inflater = popup.getMenuInflater();
+                            inflater.inflate(R.menu.include_tags, popup.getMenu());
+                            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                                @Override
+                                public boolean onMenuItemClick(MenuItem item) {
+                                    if (item.getItemId() == R.id.item_include_all) {
+                                        includeTagsInput.setHint(R.string.include_all_tags);
+                                        includeTagsFloatingLabel.setHint(context.getResources().getString(R.string.include_all_tags));
+                                    } else if (item.getItemId() == R.id.item_include_one) {
+                                        includeTagsInput.setHint(R.string.include_one_tags);
+                                        includeTagsFloatingLabel.setHint(context.getResources().getString(R.string.include_one_tags));
+                                    }
+                                    return false;
+                                }
+                            });
+                            popup.show();
+                            break;
+
+                        case MotionEvent.ACTION_CANCEL:
+                            includeTagsIcon.setAlpha(0.4f);
+                            includeTagsDropdown.setAlpha(0.4f);
+                            break;
+                    }
+                    return true;
+                }
+            });
         }
 
         return convertView;
