7285e47ca73dfabd16a667cff397a9c54c4143ae
Thomas HERBETH <thomas.herbeth@gmail.com>
2017-03-29 19:38:32 +0200

    [Fix] Fixed a graphic bug when changing theme : the splash screen newly set in the manifest for MainActivity overlapped the window background. All fragments now have a background color to avoid this bug. Also redefined the SplashScreen theme for 21+ devices to avoid status bar graphic issues (also when changing theme).

--- app/src/main/java/com/booboot/vndbandroid/activity/PreferencesFragment.java
+++ app/src/main/java/com/booboot/vndbandroid/activity/PreferencesFragment.java
@@ -7,6 +7,9 @@ import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.booboot.vndbandroid.R;
 import com.booboot.vndbandroid.model.vndbandroid.Theme;
@@ -32,6 +35,13 @@ public class PreferencesFragment extends PreferenceFragment implements Preferenc
         bindPreferenceSummaryToValue(findPreference(getActivity().getString(R.string.pref_key_nsfw)));
         bindPreferenceSummaryToValue(findPreference(getActivity().getString(R.string.pref_key_browser)));
         bindPreferenceSummaryToValue(findPreference(getActivity().getString(R.string.pref_key_cover_background)));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(getResources().getColor(android.R.color.white));
+        return view;
     }
 
     /**
