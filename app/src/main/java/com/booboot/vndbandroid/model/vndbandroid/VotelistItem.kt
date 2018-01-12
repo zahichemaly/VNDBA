5625d52e16769821a8695a58253d9f0a4d9a0a7f
Thomas HERBETH <thomas.herbeth@gmail.com>
2018-01-12 21:10:27 +0100

    [-] Removed CacheItem (data class and inheritance looks awkward).

--- app/src/main/java/com/booboot/vndbandroid/model/vndbandroid/VotelistItem.kt
+++ app/src/main/java/com/booboot/vndbandroid/model/vndbandroid/VotelistItem.kt
@@ -1,5 +1,18 @@
 package com.booboot.vndbandroid.model.vndbandroid
 
 data class VotelistItem(
+        var vn: Int = 0,
+        var added: Int = 0,
         var vote: Int = 0
-) : CacheItem()
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as VNlistItem?
+        return vn == other.vn
+    }
+
+    override fun hashCode(): Int {
+        return vn
+    }
+}
