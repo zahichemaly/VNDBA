253a47fab95838ed666a4d84c42d411a7ff4e13d
Thomas HERBETH <thomas.herbeth@gmail.com>
2018-08-02 20:17:39 +0200

    [Fix] #136 : Fixed ListRepository.getItems() not executing in a parallel way on its own thread after migrating to CachePolicy. [Fix] #136 : Fixed ListRepository.getItems() putting items in cache when it wasn't supposed to. [Fix] #136 : Fixed ListRepository.getItems() sending too many network requests and not properly checking the cache after migrating to CachePolicy.

--- app/src/main/java/com/booboot/vndbandroid/repository/ListRepository.kt
+++ app/src/main/java/com/booboot/vndbandroid/repository/ListRepository.kt
@@ -1,20 +1,27 @@
 package com.booboot.vndbandroid.repository
 
 import com.booboot.vndbandroid.model.vndb.AccountItem
+import com.booboot.vndbandroid.model.vndb.Results
 import io.reactivex.Completable
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 abstract class ListRepository<T : AccountItem> : Repository<T>() {
-    abstract fun getItemsFromDB(): List<T>
-    abstract fun addItemsToDB(items: List<T>)
+    protected abstract fun getItemsFromDB(): List<T>
+
+    protected abstract fun addItemsToDB(items: List<T>)
+
+    protected abstract fun getItemsFromAPI(): Results<T>
 
     override fun getItems(cachePolicy: CachePolicy<Map<Int, T>>): Single<Map<Int, T>> = Single.fromCallable {
         cachePolicy
             .fetchFromMemory { items }
             .fetchFromDatabase { getItemsFromDB().map { it.vn to it }.toMap() }
-            .putInMemory { items = it.toMutableMap() }
-            .get()
-    }
+            .fetchFromNetwork { getItemsFromAPI().items.map { it.vn to it }.toMap() }
+            .putInMemory { if (cachePolicy.enabled) items = it.toMutableMap() }
+            .putInDatabase { if (cachePolicy.enabled) addItemsToDB(it.values.toList()) }
+            .get { emptyMap() }
+    }.subscribeOn(Schedulers.newThread())
 
     override fun setItems(items: List<T>): Completable = Completable.fromAction {
         this.items = items.map { it.vn to it }.toMap().toMutableMap()
