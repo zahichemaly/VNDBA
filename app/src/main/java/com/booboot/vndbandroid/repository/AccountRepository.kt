67ac3e132911b6368b521b4244e1cc30d87745a1
Thomas HERBETH <thomas.herbeth@gmail.com>
2018-09-09 11:28:58 +0200

    [#] AccountRepository.getItems() now only fetches the account's VN (and not all the VNs, to optimize the upcoming database features).

--- app/src/main/java/com/booboot/vndbandroid/repository/AccountRepository.kt
+++ app/src/main/java/com/booboot/vndbandroid/repository/AccountRepository.kt
@@ -1,12 +1,12 @@
 package com.booboot.vndbandroid.repository
 
 import com.booboot.vndbandroid.model.vndb.AccountItems
-import com.booboot.vndbandroid.model.vndb.VN
 import com.booboot.vndbandroid.model.vndb.Vnlist
 import com.booboot.vndbandroid.model.vndb.Votelist
 import com.booboot.vndbandroid.model.vndb.Wishlist
+import com.booboot.vndbandroid.model.vndbandroid.FLAGS_DETAILS
 import io.reactivex.Single
-import io.reactivex.functions.Function4
+import io.reactivex.functions.Function3
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -18,12 +18,28 @@ class AccountRepository @Inject constructor(
     var votelistRepository: VotelistRepository,
     var wishlistRepository: WishlistRepository
 ) {
-    fun getItems(): Single<AccountItems> = Single.zip(
-        vnlistRepository.getItems(),
-        votelistRepository.getItems(),
-        wishlistRepository.getItems(),
-        vnRepository.getItems().subscribeOn(Schedulers.newThread()),
-        Function4<Map<Long, Vnlist>, Map<Long, Votelist>, Map<Long, Wishlist>, Map<Long, VN>, AccountItems> { vni, vti, wsi, vn ->
-            AccountItems(vni, vti, wsi, vn)
-        }).subscribeOn(Schedulers.io())
+    fun getItems(): Single<AccountItems> {
+        var items = AccountItems()
+
+        return Single.zip(
+            vnlistRepository.getItems(),
+            votelistRepository.getItems(),
+            wishlistRepository.getItems(),
+            Function3<Map<Long, Vnlist>, Map<Long, Votelist>, Map<Long, Wishlist>, AccountItems> { vni, vti, wsi ->
+                items = AccountItems(vni, vti, wsi)
+                items
+            })
+            .observeOn(Schedulers.newThread())
+            .flatMap {
+                vnRepository.getItems(
+                    ids = it.vnlist.keys.union(it.votelist.keys).union(it.wishlist.keys),
+                    flags = FLAGS_DETAILS
+                )
+            }
+            .map {
+                items.vns = it
+                items
+            }
+            .subscribeOn(Schedulers.io())
+    }
 }
