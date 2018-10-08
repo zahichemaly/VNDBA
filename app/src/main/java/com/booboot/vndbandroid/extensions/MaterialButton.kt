1b5d245efeae8d9e0488cef50730379eb8fb2a24
Thomas HERBETH <thomas.herbeth@showroomprive.net>
2018-10-08 13:52:27 +0200

    [+] #147 : extension functions to select and unselect an outlined button

---
+++ app/src/main/java/com/booboot/vndbandroid/extensions/MaterialButton.kt
@@ -0,0 +1,19 @@
+package com.booboot.vndbandroid.extensions
+
+import android.content.res.ColorStateList
+import androidx.core.content.ContextCompat
+import com.booboot.vndbandroid.R
+import com.booboot.vndbandroid.util.Pixels
+import com.google.android.material.button.MaterialButton
+
+fun MaterialButton.select() = apply {
+    setTextColor(ContextCompat.getColor(context, R.color.white))
+    backgroundTintList = strokeColor
+    elevation = Pixels.px(2).toFloat()
+}
+
+fun MaterialButton.unselect() = apply {
+    setTextColor(strokeColor)
+    backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(context, android.R.color.transparent))
+    elevation = Pixels.px(0).toFloat()
+}
