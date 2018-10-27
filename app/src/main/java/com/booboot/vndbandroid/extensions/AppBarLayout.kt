20ca134f22588145c3b6291ed8c2ff0ae291fbc7
Thomas HERBETH <thomas.herbeth@gmail.com>
2018-10-27 12:17:56 +0200

    [#] #52 : added back an elevation on the home toolbar. [#] #52 : VN details' toolbar background color is now windowBackground too. [Fix] #52 : in VN details, in dark mode, the status bar no longer keeps its translucency when the toolbar is collapsing.

--- app/src/main/java/com/booboot/vndbandroid/extensions/AppBarLayout.kt
+++ app/src/main/java/com/booboot/vndbandroid/extensions/AppBarLayout.kt
@@ -11,23 +11,27 @@ fun AppBarLayout.setStatusBarThemeForCollapsingToolbar(activity: AppCompatActivi
     addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, offset ->
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return@OnOffsetChangedListener
 
-        if (activity.dayNightTheme() != "light") return@OnOffsetChangedListener
-
         if (collapsingToolbar.height + offset < collapsingToolbar.scrimVisibleHeightTrigger) {
             /* Toolbar is collapsing: removing the status bar's translucence and adding the light flag */
             activity.window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            if (activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR == 0) {
-                activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                /* Scroll is broken if we don't refresh layout on the sibling view for some reason */
-                contentView.requestLayout()
+
+            if (activity.dayNightTheme() == "light") {
+                if (activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR == 0) {
+                    activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    /* Scroll is broken if we don't refresh layout on the sibling view for some reason */
+                    contentView.requestLayout()
+                }
             }
         } else {
             /* Toolbar is expanding: adding the status bar's translucence and removing the light flag */
             activity.window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            if (activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR != 0) {
-                activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-                /* Scroll is broken if we don't refresh layout on the sibling view for some reason */
-                contentView.requestLayout()
+
+            if (activity.dayNightTheme() == "light") {
+                if (activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR != 0) {
+                    activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                    /* Scroll is broken if we don't refresh layout on the sibling view for some reason */
+                    contentView.requestLayout()
+                }
             }
         }
     })
