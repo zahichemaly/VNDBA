6caf9fb511c05150fccc27a57a33c12a9e0d20db
Thomas HERBETH <vndba.app@gmail.com>
2019-04-13 14:28:54 +0200

    [+] coroutine() helper function in BaseViewModel, for easy-to-use coroutines which cover all use cases. :)

--- app/src/main/java/com/booboot/vndbandroid/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/booboot/vndbandroid/ui/base/BaseViewModel.kt
@@ -5,19 +5,44 @@ import android.os.Bundle
 import android.os.Parcelable
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.booboot.vndbandroid.extensions.errorMessage
 import com.booboot.vndbandroid.extensions.log
+import com.booboot.vndbandroid.extensions.minus
+import com.booboot.vndbandroid.extensions.plus
 import com.booboot.vndbandroid.util.EmptyMaybeException
 import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel constructor(application: Application) : AndroidViewModel(application) {
-    val loadingData: MutableLiveData<Int> = MutableLiveData()
-    val errorData: MutableLiveData<String> = MutableLiveData()
-    val disposables: MutableMap<String, Disposable> = mutableMapOf()
+    val loadingData = MutableLiveData<Int>()
+    val errorData = MutableLiveData<String>()
+    val disposables = mutableMapOf<String, Disposable>()
+    private val jobs = mutableMapOf<String, Job>()
+    private val errorHandler = CoroutineExceptionHandler { _, throwable ->
+        onError(throwable)
+    }
 
     /* State saving #2 : when coming back to the Fragment with the NavController (onSaveInstanceState not called) */
     var hasPendingTransition = false
     var layoutState: Parcelable? = null
+
+    fun coroutine(jobName: String, errorHandler: CoroutineExceptionHandler = this.errorHandler, block: suspend CoroutineScope.() -> Unit) {
+        if (jobName in jobs) return
+
+        loadingData.plus()
+        jobs[jobName] = viewModelScope.launch(errorHandler) {
+            block()
+        }.apply {
+            invokeOnCompletion {
+                loadingData.minus()
+                jobs.remove(jobName)
+            }
+        }
+    }
 
     fun onError(throwable: Throwable) {
         onError(throwable, errorData)
@@ -29,6 +54,7 @@ abstract class BaseViewModel constructor(application: Application) : AndroidView
         errorData.value = throwable.errorMessage()
     }
 
+    // TODO remove : no longer useful with viewModelScope
     override fun onCleared() {
         disposables.forEach { it.value.dispose() }
     }
