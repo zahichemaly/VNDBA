b559ddb73c5353a4423412685c546534c66bf3c4
Thomas HERBETH <vndba.app@gmail.com>
2019-12-28 19:08:35 +0100

    [Fix] Fixed #173 : relations are now in the correct order.

--- app/src/main/java/com/booboot/vndbandroid/ui/vnrelations/RelationsViewModel.kt
+++ app/src/main/java/com/booboot/vndbandroid/ui/vnrelations/RelationsViewModel.kt
@@ -7,6 +7,7 @@ import com.booboot.vndbandroid.App
 import com.booboot.vndbandroid.diff.RelationsDiffCallback
 import com.booboot.vndbandroid.extensions.plusAssign
 import com.booboot.vndbandroid.model.vndb.AccountItems
+import com.booboot.vndbandroid.model.vndb.RELATIONS
 import com.booboot.vndbandroid.model.vndb.VN
 import com.booboot.vndbandroid.model.vndbandroid.FLAGS_DETAILS
 import com.booboot.vndbandroid.repository.AccountRepository
@@ -31,6 +32,7 @@ class RelationsViewModel constructor(application: Application) : BaseViewModel(a
         val vn = vnJob.await()
         val accountItemsCopy = accountItems.deepCopy()
         val relations = vnRepository.getItems(vn.relations.mapTo(hashSetOf()) { it.id }, FLAGS_DETAILS)
+        vn.relations = vn.relations.sortedWith(compareBy { RELATIONS.keys.indexOf(it.relation) })
         relationsData += RelationsData(vn, accountItemsCopy, relations).apply {
             val diffCallback = RelationsDiffCallback(oldRelationsData, this)
             diffResult = DiffUtil.calculateDiff(diffCallback)
