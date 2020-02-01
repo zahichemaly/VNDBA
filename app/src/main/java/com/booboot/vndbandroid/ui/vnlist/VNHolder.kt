7fa2f721a518824b8b5afa83b530e37107f2d426
Thomas HERBETH <vndba.app@gmail.com>
2020-02-01 15:16:43 +0100

    [#] Simplified VNHolder with LayoutContainer.

--- app/src/main/java/com/booboot/vndbandroid/ui/vnlist/VNHolder.kt
+++ app/src/main/java/com/booboot/vndbandroid/ui/vnlist/VNHolder.kt
@@ -9,17 +9,12 @@ import com.booboot.vndbandroid.model.vndb.UserList
 import com.booboot.vndbandroid.model.vndb.VN
 import com.booboot.vndbandroid.model.vndbandroid.Vote
 import com.booboot.vndbandroid.util.Utils
-import kotlinx.android.synthetic.main.nsfw_tag.view.*
-import kotlinx.android.synthetic.main.vn_card.view.*
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.nsfw_tag.*
+import kotlinx.android.synthetic.main.vn_card.*
 
-class VNHolder(itemView: View, private val onVnClicked: (View, VN) -> Unit) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-    private lateinit var vn: VN
-
-    init {
-        itemView.cardView.setOnClickListener(this)
-    }
-
-    fun onBind(
+class VNHolder(override val containerView: View, private val onVnClicked: (View, VN) -> Unit) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    fun bind(
         vn: VN,
         userList: UserList?,
         showFullDate: Boolean,
@@ -27,13 +22,13 @@ class VNHolder(itemView: View, private val onVnClicked: (View, VN) -> Unit) : Re
         showRating: Boolean,
         showPopularity: Boolean,
         showVoteCount: Boolean
-    ) = with(itemView) {
-        this@VNHolder.vn = vn
+    ) {
+        cardView.setOnClickListener { onVnClicked(containerView, vn) }
 
         val titleText = StringBuilder()
         val subtitleText = StringBuilder()
         if (showRank)
-            titleText.append("#").append(adapterPosition + 1).append(context.getString(R.string.dash))
+            titleText.append("#").append(adapterPosition + 1).append(containerView.context.getString(R.string.dash))
         titleText.append(vn.title)
         when {
             showRating -> subtitleText.append(vn.rating).append(" (").append(Vote.getName(vn.rating)).append(")")
@@ -42,7 +37,7 @@ class VNHolder(itemView: View, private val onVnClicked: (View, VN) -> Unit) : Re
             else -> subtitleText.append(Utils.getDate(vn.released, showFullDate))
         }
 
-        subtitleText.append(context.getString(R.string.bullet))
+        subtitleText.append(containerView.context.getString(R.string.bullet))
         if (vn.length ?: 0 > 0)
             subtitleText.append(vn.lengthFull())
         else
@@ -56,9 +51,5 @@ class VNHolder(itemView: View, private val onVnClicked: (View, VN) -> Unit) : Re
         statusButton.text = Label.toShortString(userList?.firstStatus()?.id)
         wishlistButton.text = Label.toShortString(userList?.firstWishlist()?.id)
         votesButton.text = Vote.toShortString(userList?.vote)
-    }
-
-    override fun onClick(v: View?) {
-        onVnClicked(itemView, vn)
     }
 }
