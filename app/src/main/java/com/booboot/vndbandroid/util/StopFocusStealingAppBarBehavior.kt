1993593303558446f91efe6c7db232e907003a18
Thomas HERBETH <vndba.app@gmail.com>
2020-02-08 23:08:44 +0100

    [#] Decreased bottom sheet peek to 60dp. [Fix] Fixed StopFocusStealingAppBarBehavior to take 1st-level children into account.

--- app/src/main/java/com/booboot/vndbandroid/util/StopFocusStealingAppBarBehavior.kt
+++ app/src/main/java/com/booboot/vndbandroid/util/StopFocusStealingAppBarBehavior.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.children
 import com.google.android.material.appbar.AppBarLayout
 
 internal class StopFocusStealingAppBarBehavior : AppBarLayout.Behavior {
@@ -19,7 +21,10 @@ internal class StopFocusStealingAppBarBehavior : AppBarLayout.Behavior {
 
     override fun onStartNestedScroll(parent: CoordinatorLayout, child: AppBarLayout, directTargetChild: View, target: View, nestedScrollAxes: Int, type: Int): Boolean {
         // Set flag if the bottom sheet is responsible for the nested scroll.
-        isAboveViewTouched = target in aboveViews
+        isAboveViewTouched = target in aboveViews || target in aboveViews.flatMap {
+            if (it is ViewGroup) it.children.toList() else listOf()
+        }
+
         // Only consider starting a nested scroll if the bottom sheet is not touched; otherwise,
         // we will let the other views do the scrolling.
         return !isAboveViewTouched && super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes, type)
